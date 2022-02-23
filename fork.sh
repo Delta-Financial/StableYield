706e86ad90f7d52015e01c4b486ebffc44765a42
x3devships <x3dev@protonmail.com>
2022-02-23 12:26:44 -0600

    stable yieldz

---
+++ fork.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+forge test --fork-url https://mainnet.infura.io/v3/10c6bca900c64bb189de392c896e09b6 -vvvv
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,11 +2,25 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "./Vm/CheatCodes.sol";
+import "../StableYield.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    IERC20 constant DELTA = IERC20(0x9EA3b5b4EC044b70375236A281986106457b20EF);
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    StableYield sYield;
+    function setUp() public {
+        sYield = new StableYield();
+    }
 
     function testExample() public {
+        vm.startPrank(0x5A16552f59ea34E44ec81E58b3817833E9fD5436);
+        sYield.enableWithDefaults();
+
+        // supply with some delta
+        DELTA.transfer(address(sYield), 90000e18);
+
+        sYield.distribute();
         assertTrue(true);
     }
 }
